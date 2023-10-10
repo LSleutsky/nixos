8a1ad2a016c99c3789676ad6a41a790c30021139
LSleutsky <LushSleutsky@gmail.com>
2023-10-10 10:43:23 -0400

    chore: hyprland systemd integration key

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    systemdIntegration = true;
+    systemd.enable = true;
     settings = {
       "$mainMod" = "SUPER";
       "$mainModShift" = "SUPERSHIFT";
