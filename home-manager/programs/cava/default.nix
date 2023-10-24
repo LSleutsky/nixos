a36299ac654767af4f7927bec4801d5746ce0849
LSleutsky <LushSleutsky@gmail.com>
2023-10-24 07:28:43 -0400

    style: `cava` gradient colors config

--- home-manager/programs/cava/default.nix
+++ home-manager/programs/cava/default.nix
@@ -18,14 +18,11 @@
       };
       color = {
         gradient = 1;
-        gradient_color_1 = "'#f7768e'";
-        gradient_color_2 = "'#9ece6a'";
-        gradient_color_3 = "'#e0af68'";
+        gradient_color_1 = "'#9ece6a'";
+        gradient_color_2 = "'#73daca'";
+        gradient_color_3 = "'#7dcfff'";
         gradient_color_4 = "'#7aa2f7'";
         gradient_color_5 = "'#bb9af7'";
-        gradient_color_6 = "'#7dcfff'";
-        gradient_color_7 = "'#73daca'";
-        gradient_color_8 = "'#c0caf5'";
       };
       smoothing = {
         waves = 0;
